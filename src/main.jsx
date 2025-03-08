2c9b15a2a493c08803e923a9b32986e94e22bf80
logicless-bt <benpturner01@gmail.com>
2025-03-07 19:23:47 -0500

    service worker and manifest
    - created manifest.json, service-worker.js, and serviceWorkerRegistration.js

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import * as atatus from 'atatus-spa';
-atatus.config('57a8e02211894e6aa1b7f02473b932bd').install();
+//import * as atatus from 'atatus-spa';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+//atatus.config('57a8e02211894e6aa1b7f02473b932bd').install();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+serviceWorkerRegistration.register();
